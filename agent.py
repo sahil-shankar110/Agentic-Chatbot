dc4e5a1bd84678dec0b08b94934779826b2023f2
sahil-shankar110 <sahilshankar431@gmail.com>
2026-03-30 11:35:27 +0500

    Change some code

--- agent.py
+++ agent.py
@@ -10,7 +10,6 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 from langchain_groq import ChatGroq
-from langchain_openrouter import ChatOpenRouter
 from langchain_tavily import TavilySearch
 from langchain.agents import create_agent
 from langchain_core.messages import AIMessage
@@ -19,9 +18,9 @@ from langchain_core.messages import AIMessage
 def get_agent_response(llm_id , allow_search, system_prompt, provider, query):
 
     if provider == "groq":
-        llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY , temperature=0.7 , max_tokens=2048)
+        llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY ,temperature=0.4,max_tokens=None,max_retries=2)
     elif provider == "openai":
-        llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY , temperature=0.7 , max_tokens=2048)
+        llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY , temperature=0.4,max_tokens=None,max_retries=2)
     else:
         raise ValueError(f"Unknown provider: {provider}")
    
--- backend.py
+++ backend.py
@@ -31,3 +31,5 @@ def chat(request: RequestModel):
     
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# uvicorn backend:app --reload
